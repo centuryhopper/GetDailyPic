3289d4dca26c89f0f0bd0f200627b08fa739398c
Leo Zhang <leotheasianlion@gmail.com>
2022-05-23 09:42:55 -0400

    update
    modified the linux change desktop background command to be friendly with either dark or light themes

--- get_daily_pic.py
+++ get_daily_pic.py
@@ -3,12 +3,12 @@ import platform
 import pwd
 import os
 import time
+import darkdetect
 from secrets import Secrets
 
 url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 currentTime = time.strftime("%Y-%m-%d-%H-%M-%S")
 FILENAME = f'nasa_pic_{currentTime}.png'
-
 
 def get_filename():
     username = pwd.getpwuid(os.getuid()).pw_name
@@ -53,10 +53,13 @@ def main():
     download_pic_of_day()
 
     filename = get_filename()
+    print(filename)
 
     # set background
     if platform.system()=="Linux":
-        cmd = "gsettings set org.gnome.desktop.background picture-uri file:" + filename
+        # append -dark if the os theme is dark
+        darkText = '-dark' if darkdetect.isDark() else ''
+        cmd = f"gsettings set org.gnome.desktop.background picture-uri{darkText} file://" + filename
     elif platform.system()=="Darwin":
         cmd = "osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"" + filename +"\"'"
         # use absolute path to the image, and not a path that begins with a user path (~/Downloads/image.jpg)!
